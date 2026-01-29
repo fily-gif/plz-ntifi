e3ded92f92fcce0eeb2bf5d677fcf80faad477a1
fily-gif <zeroad69@gmail.com>
2026-01-29 17:22:36 -0500

    moved stuff around, better valid server detection (now with a global server variable!)

--- ntifi/jellyfin-api.py
+++ ntifi/jellyfin-api.py
@@ -1,8 +1,9 @@
 from config import *
 import json
 import requests
+server = Jellyfin.server if Jellyfin.server.startswith(("http://", "https://")) else f"https://{Jellyfin.server}"
+#* assuming https
 
-global storage
 def auth(token:str=None, username=None, password=None):
 	# Takes in either `token`, or (`username` AND `password`).
 	# If only token provided (api key), tries to validate it via `GET /System/Info`. on fail, throws and screams.
@@ -12,21 +13,16 @@ def auth(token:str=None, username=None, password=None):
 	# TODO: move to global scope?
 	sess = requests.Session()
 	# is the server even valid?
-	server_is_valid: bool = False
-	server_is_valid = True if sess.get(f"{Jellyfin.server}/System/Info/Public").status_code == 200 else False
+	server_is_valid: bool = sess.get(f"{server}/System/Info/Public").status_code == 200
 	if server_is_valid:
-		if token and not (username and password): # why is python like that.
-			print("using token to log in...")
+		if token and not (username and password):
+			print(f"using token to log in for {server}...")
 			token = f'MediaBrowser Token="{token}"'
 			sess.headers.update({'User-Agent': "fily-github-com/1.0", "Authorization": token})
 			try:
-				test = sess.get(f"{Jellyfin.server}/System/Info")
+				test = sess.get(f"{server}/System/Info")
 				print(test.json())
 			except Exception as e:
-				#! VVV broken!!
-				# if "No scheme supplied" in e:
-				# 	Jellyfin.server = f"https://{Jellyfin.server}" # assuming https because why would it Not be https..?
-				# 	raise
 				print(e)
 				#raise "JellyfinAuthException"
 		pass
