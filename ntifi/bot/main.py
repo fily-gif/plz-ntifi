b9d38c260bbb8b74c45de8884ea3b2cd466ff71d
fily-gif <zeroad69@gmail.com>
2026-02-04 22:21:59 -0500

    basic bot ready!

--- ntifi/bot/main.py
+++ ntifi/bot/main.py
@@ -1,12 +1,20 @@
-import discord
-from discord.ext import commands
-from ...config import Discord
+import os
+import nextcord
+from nextcord.ext import commands
+#from discord import app_commands # slash commands
+from dotenv import load_dotenv
 
-intents = discord.Intents.all()
-bot = commands.Bot()
+load_dotenv()
+intents = nextcord.Intents.all()
+bot = commands.Bot(intents=intents)
+token = os.getenv("bot_token")
 
-@bot.command()
+@bot.slash_command()
 async def ping(ctx):
 	await ctx.send('pong')
 
-bot.run
+@bot.event
+async def on_ready():
+	print(f"logged in as {bot.user}!! ({bot.user.id})")
+
+bot.run(token)
